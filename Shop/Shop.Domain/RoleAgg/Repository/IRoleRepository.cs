827b0c487dc5e5181fcca851f2558509563a3cc0
Amirmhdigol <amirmhdigol82@gmail.com>
2022-04-17 14:21:08 +0430

    IBaseRepositories Added And Domain Completed

---
+++ Shop/Shop.Domain/RoleAgg/Repository/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Common.Domain.Repository;
+using Shop.Domain.OrderAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.RoleAgg.Repository
+{
+    public interface IRoleRepository : IBaseRepository<Role>
+    {
+    }
+}
